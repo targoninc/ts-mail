f7db669c7e39f28456fcffa06f95fcfd34975af7
alex <alex@targoninc.com>
2026-02-01 15:38:49 +0100

    export image

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {MailBuilder, card, link, heading, paragraph} from "./Mail/MailBuilder";
+export {MailBuilder, card, link, image, heading, paragraph} from "./Mail/MailBuilder";
 export {Mail, Email} from "./Mail/Mail";
 
 export type {MailElement} from "./Mail/MailElement.ts";
